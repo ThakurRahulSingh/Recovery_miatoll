95bf7525bcae1de0ec5745d53399c2742f7b45c8
Darth9 <reyprimerjefe@gmail.com>
2021-06-13 19:24:48 +0100

    [WiP] Initial commit for 11.0 the manifest

--- device.mk
+++ device.mk
@@ -40,4 +40,20 @@ PRODUCT_PACKAGES += \
 # OEM otacert
 PRODUCT_EXTRA_RECOVERY_KEYS += \
     vendor/recovery/security/miui
+
+
+# for Android 11 manifests
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/commonsys-intf/display
+
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+TW_EXCLUDE_APEX := true
+
+TARGET_RECOVERY_DEVICE_MODULES += libion libandroidicu vendor.display.config@1.0 vendor.display.config@2.0 libdisplayconfig.qti
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so
 #
