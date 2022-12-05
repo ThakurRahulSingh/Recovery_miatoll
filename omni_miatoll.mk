c8922af150659db44a4b7c656460b847b5be9482
Rahul Singh <bhadoriyarahul.180@gmail.com>
2022-12-05 16:28:19 +0530

    Update and rename twrp_miatoll.mk to omni_miatoll.mk

--- omni_miatoll.mk
+++ omni_miatoll.mk
@@ -22,20 +22,23 @@ DEVICE_PATH := device/xiaomi/miatoll
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
 # for the amended f2fs command
 # casefolding causes encryption problems with f2fs formatting on Android 12
 # so disable this
 # $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := miatoll
-PRODUCT_NAME := twrp_miatoll
+PRODUCT_NAME := omni_miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 9S
 PRODUCT_MANUFACTURER := Xiaomi
